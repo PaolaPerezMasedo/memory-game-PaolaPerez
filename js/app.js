6a3728ea723e068fe8d357fc37253b8b38353ce5
Paola TesoritoLi <paola.perez.masedo@gmail.com>
2018-05-19 15:33:59 +0200

    fixed the array of cards and erased the static html of the cards

--- js/app.js
+++ js/app.js
@@ -1,10 +1,81 @@
-//array of objects from the deck
- let array_cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+//a list that holds all the cards
+ const array_cards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
 
- const deck = document.getElementById('deck');
- const timer= document.getElementByClassName('timer');
- const moves= document.getElementByClassName('moves');
+
+ let moves= document.getElementByClassName('moves');
+ let starRating= document.getElementByClassName('fa fa-star');
  let cardsOpen= 0;
+ let cardValues=[];
+ let cardId= [];
+ let stars1= 12;
+ let stars2= 20;
+ let stars3= 22;
+
+
+
+const deck = document.querySelector(".deck");
+function startGame(){
+   let shuffledArray = shuffle(array_cards);
+   for (var i= 0; i < shuffledArray.length; i++){
+     let li = document.createElement("li");
+     let text = document.createTextNode(shuffledArray[i]);
+     li.appendChild(text);
+     deck.appendChild(li);
+}
+window.onload=startGame();
+
+
+
+
+
+
+//timer
+let second = 0, minute = 0;
+let timer = document.querySelector(".timer");
+let interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
+}
+
+//counter
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    if(moves == 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        startTimer();
+    }
+
+//Star Rating
+function rating(moves){
+let rating=3;
+if( moves > stars3 && moves < stars2){
+  starRating.eq(3).removeClass('fa fa-star');
+} else if (moves > stars2 && moves < stars1){
+  starRating.eq(2).removeClass('fa fa star');
+  rating=2;
+} else if (moves > stars1){
+  starRating.eq(1).removeClass('fa fa-star');
+  rating=1;
+}
+ return ( "Your score is" + rating);
+
+}
+
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -22,21 +93,6 @@ function shuffle(array) {
 }
 
 
-//start Game
-function startGame(){
-   let shuffledArray = shuffle(array_cards);
-   cardsOpen= 0;
-   timer= 0;
-   moves=0;
-   for (var i= 0; i < shuffledArray.length; i++){
-     let li = document.createElement("li");
-     let text = document.createTextNode(shuffledArray[i]);
-     li.appendChild(text);
-     document.getElementById("deck").appendChild(li);
-}
-})
-}
-window.addEventListener = startGame();
 
 
 /*
